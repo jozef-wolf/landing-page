693fe1331d7f36f09b39363dedffc2e4ad0589df
jozef-wolf <jozefwolf@o2.pl>
2021-03-25 10:05:27 +0100

    css files deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
