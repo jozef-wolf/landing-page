727db49b664b9cb0b9ddf68143f94d71fe465eeb
jozef-wolf <jozefwolf@o2.pl>
2021-03-25 10:15:28 +0100

    Navbar.jsx created

---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="logo">Nav bar</div>
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact Us</li>
+        <li>Sign In</li>
+        <li>Sign Up</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
