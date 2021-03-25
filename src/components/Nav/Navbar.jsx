a7a8c484f7529ddccf748723aed52437768155c0
jozef-wolf <jozefwolf@o2.pl>
2021-03-25 10:21:12 +0100

    Navbar structure styled

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 70px;
+  border-bottom: 1px solid black;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+    padding: 15px 0;
+  }
+  ul {
+    list-style: none;
+    display: flex;
+    flex-flow: row nowrap;
+
+    li {
+      padding: 20px 10px;
+    }
+  }
+`;
 
 const Navbar = () => {
   return (
-    <div>
+    <Nav>
       <div className="logo">Nav bar</div>
       <ul>
         <li>Home</li>
@@ -11,7 +34,7 @@ const Navbar = () => {
         <li>Sign In</li>
         <li>Sign Up</li>
       </ul>
-    </div>
+    </Nav>
   );
 };
 
