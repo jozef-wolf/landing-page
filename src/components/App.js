20a6c6a8a89989f903fa1d7d2aecf3597d376f60
jozef-wolf <jozefwolf@o2.pl>
2021-03-26 09:44:33 +0100

    Footer created

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Nav/Navbar";
 import HeaderSection from "./HeaderSection";
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeaderSection />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Foot = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 10px 0 0 10px;
+`;
+
+const Footer = () => {
+  return (
+    <Foot>
+      <div>© Józef Wolf</div>
+    </Foot>
+  );
+};
+
+export default Footer;
